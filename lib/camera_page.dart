f351d32b78b8ee7f94c188e13af697629a187100
adi <adiandoyo.dl32@gmail.com>
2021-05-19 17:01:29 +0700

    first commit

---
+++ lib/camera_page.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class CameraPage extends StatefulWidget {
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  CameraController _controller;
+
+  Future<void> _initializeCamera() async {
+    var cameras = await availableCameras();
+    _controller = CameraController(cameras[0], ResolutionPreset.medium);
+    await _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<File> _takePicture() async {
+    Directory root = await getTemporaryDirectory();
+    String directoryPath = '${root.path}/guide_camera';
+    await Directory(directoryPath).create(recursive: true);
+    String filePath = '$directoryPath/${DateTime.now()}.jpg';
+
+    try {
+      await _controller.takePicture(filePath);
+    } catch (e) {
+      return null;
+    }
+    return File(filePath);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take Picture')),
+      backgroundColor: Colors.black12,
+      body: FutureBuilder<void>(
+        future: _initializeCamera(),
+        builder: (_, snapshot) =>
+            (snapshot.connectionState == ConnectionState.done)
+                ? Stack(
+                    children: [
+                      Column(
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.width / _controller.value.aspectRatio,
+                            child: CameraPreview(_controller),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                : Center(
+                    child: SizedBox(
+                      height: 20,
+                      width: 20,
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_guide_camera/camera_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: (imageFile != null) ? Image.file(imageFile) : SizedBox(),
+            ),
+            RaisedButton(
+              child: Text('Take Picture'),
+              onPressed: () async {
+                imageFile = await Navigator.push<File>(
+                    context, MaterialPageRoute(builder: (_) => CameraPage()));
+                setState(() {});
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
